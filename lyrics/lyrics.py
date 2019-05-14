8a2dadb082213ee8c794c93638cf6e35fbb5ab41
Wyn <Wyn10@users.noreply.github.com>
2019-05-14 04:49:45 -0400

    Add files via upload

--- lyrics/lyrics.py
+++ lyrics/lyrics.py
@@ -2,12 +2,12 @@ import re
 
 import discord
 import lavalink
-import urllib3
+import requests
 from bs4 import BeautifulSoup
 from redbot.core import commands
+from redbot.core.utils.chat_formatting import pagify
 
 BaseCog = getattr(commands, "Cog", object)
-
 color = discord.colour.Color.dark_blue()
 
 
@@ -33,11 +33,14 @@ class Lyrics(BaseCog):
 
         try:
             results = lyrics_musixmatch(artistsong)
-            return await ctx.send(
-                '**__Lyrics for__** `' + artistsong + '`, Requested by {}'.format(ctx.message.author.mention),
-                embed=discord.Embed(description=results, colour=color))
-        except discord.HTTPException:
-            return await ctx.send(embed=discord.Embed(description="No lyrics Found..", colour=color))
+
+            for page in pagify(results):
+                e = discord.Embed(title='Lyrics for __{}__'.format(artistsong), description=page, colour=color)
+                e.set_footer(text='Requested by {}'.format(ctx.message.author))
+                await ctx.send(embed=e)
+
+        except discord.HTTPException as e:
+            return await ctx.send(embed=discord.Embed(description="{}".format(e), colour=color))
 
     @lyrics.command()
     async def playing(self, ctx):
@@ -62,9 +65,11 @@ class Lyrics(BaseCog):
 
         try:
             results = lyrics_musixmatch(botsong)
-            return await ctx.send(
-                '**__Lyrics for__** `' + botsong + '`' + ', Requested by {}'.format(ctx.message.author.mention),
-                embed=discord.Embed(description=results, colour=color))
+
+            for page in pagify(results):
+                e = discord.Embed(title='Lyrics for __{}__'.format(botsong), description=page, colour=color)
+                e.set_footer(text='Requested by {}'.format(ctx.message.author))
+                await ctx.send(embed=e)
         except discord.HTTPException:
             return await ctx.send(embed=discord.Embed(description="No lyrics Found..", colour=color))
 
@@ -72,13 +77,13 @@ class Lyrics(BaseCog):
 def lyrics_musixmatch(artistsong):
     artistsong = re.sub('[^a-zA-Z0-9 \n.]', '', artistsong)
     artistsong = re.sub(r'\s+', ' ', artistsong).strip()
+    headers = {'User-Agent': 'Mozilla/5.0 (X11; Arch Linux; Linux x86_64; rv:66.0) Gecko/20100101 Firefox/66.0'}
     try:
-        searchurl = "https://www.musixmatch.com/search/{}".format(artistsong).replace(" ", "%20")
-        http = urllib3.PoolManager()
-        searchresult = http.request('GET', searchurl)
-        soup = BeautifulSoup(searchresult.data, 'html.parser')
+        searchresult = requests.get("https://musixmatch.com/search/{}".format(artistsong).replace(" ", "%20"),
+                                    headers=headers)
+        soup = BeautifulSoup(searchresult.content, 'html.parser')
         url = "https://www.musixmatch.com" + soup.find('a', {"class": "title"})['href']
-        soup = BeautifulSoup(http.request("GET", url).data, 'html.parser')
+        soup = BeautifulSoup(requests.get(url, headers=headers).content, 'html.parser')
         lyrics = soup.text.split('"body":"')[1].split('","language"')[0]
         lyrics = lyrics.replace("\\n", "\n")
         lyrics = lyrics.replace("\\", "")
@@ -87,6 +92,6 @@ def lyrics_musixmatch(artistsong):
         lyrics = lyrics.strip()
 
     except Exception:
-        lyrics = "No lyrics Found."
+        lyrics = 'No lyrics Found.'
 
     return lyrics
