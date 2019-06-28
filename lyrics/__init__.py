38b76550d9a199eecbc73bbbace99e73f1754716
Wyn <Wyn10@users.noreply.github.com>
2019-06-27 23:30:43 -0400

    Add files via upload

--- lyrics/__init__.py
+++ lyrics/__init__.py
@@ -3,4 +3,3 @@ from .lyrics import Lyrics
 
 def setup(bot):
     bot.add_cog(Lyrics(bot))
-    
--- lyrics/lyrics.py
+++ lyrics/lyrics.py
@@ -2,13 +2,12 @@ import re
 
 import discord
 import lavalink
-import requests
 from bs4 import BeautifulSoup
 from redbot.core import commands
 from redbot.core.utils.chat_formatting import pagify
+from requests_futures.sessions import FuturesSession
 
 BaseCog = getattr(commands, "Cog", object)
-color = discord.colour.Color.dark_blue()
 
 
 class Lyrics(BaseCog):
@@ -35,12 +34,13 @@ class Lyrics(BaseCog):
             results = lyrics_musixmatch(artistsong)
 
             for page in pagify(results):
-                e = discord.Embed(title='Lyrics for __{}__'.format(artistsong), description=page, colour=color)
+                e = discord.Embed(title='Lyrics for __{}__'.format(artistsong), description=page,
+                                  colour=await ctx.embed_color())
                 e.set_footer(text='Requested by {}'.format(ctx.message.author))
                 await ctx.send(embed=e)
 
         except discord.HTTPException as e:
-            return await ctx.send(embed=discord.Embed(description="{}".format(e), colour=color))
+            return await ctx.send(embed=discord.Embed(description="{}".format(e), colour=await ctx.embed_color()))
 
     @lyrics.command()
     async def playing(self, ctx):
@@ -57,21 +57,25 @@ class Lyrics(BaseCog):
                     r"((\[)|(\()).*(of?ficial|feat\.?|ft\.?|audio|video|lyrics?|remix|HD).*(?(2)\]|\))",
                     flags=re.I).sub('', player.current.title).strip()
             except AttributeError:
-                return await ctx.send(embed=discord.Embed(description="Nothing playing.", colour=color))
+                return await ctx.send(
+                    embed=discord.Embed(description="Nothing playing.", colour=await ctx.embed_color()))
             except KeyError:
-                return await ctx.send(embed=discord.Embed(description="Nothing playing.", colour=color))
+                return await ctx.send(
+                    embed=discord.Embed(description="Nothing playing.", colour=await ctx.embed_color()))
         else:
-            return await ctx.send(embed=discord.Embed(description="Audio not loaded.", colour=color))
+            return await ctx.send(embed=discord.Embed(description="Audio not loaded.", colour=await ctx.embed_color()))
 
         try:
             results = lyrics_musixmatch(botsong)
 
             for page in pagify(results):
-                e = discord.Embed(title='Lyrics for __{}__'.format(botsong), description=page, colour=color)
+                e = discord.Embed(title='Lyrics for __{}__'.format(botsong), description=page,
+                                  colour=await ctx.embed_color())
                 e.set_footer(text='Requested by {}'.format(ctx.message.author))
                 await ctx.send(embed=e)
-        except discord.HTTPException:
-            return await ctx.send(embed=discord.Embed(description="No lyrics Found..", colour=color))
+        except discord.Forbidden:
+            return await ctx.send(
+                embed=discord.Embed(description="Missing embed permissions..", colour=await ctx.embed_color()))
 
 
 def lyrics_musixmatch(artistsong):
@@ -79,11 +83,12 @@ def lyrics_musixmatch(artistsong):
     artistsong = re.sub(r'\s+', ' ', artistsong).strip()
     headers = {'User-Agent': 'Mozilla/5.0 (X11; Arch Linux; Linux x86_64; rv:66.0) Gecko/20100101 Firefox/66.0'}
     try:
-        searchresult = requests.get("https://musixmatch.com/search/{}".format(artistsong).replace(" ", "%20"),
-                                    headers=headers)
-        soup = BeautifulSoup(searchresult.content, 'html.parser')
+        session = FuturesSession()
+        searchresult = session.get("https://musixmatch.com/search/{}".format(artistsong).replace(" ", "%20"),
+                                   headers=headers)
+        soup = BeautifulSoup(searchresult.result().content, 'html.parser')
         url = "https://www.musixmatch.com" + soup.find('a', {"class": "title"})['href']
-        soup = BeautifulSoup(requests.get(url, headers=headers).content, 'html.parser')
+        soup = BeautifulSoup(session.get(url, headers=headers).result().content, 'html.parser')
         lyrics = soup.text.split('"body":"')[1].split('","language"')[0]
         lyrics = lyrics.replace("\\n", "\n")
         lyrics = lyrics.replace("\\", "")
